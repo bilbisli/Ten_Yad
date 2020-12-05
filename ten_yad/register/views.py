46d8795031cedc579f13715476e6923245f64cda
israel avihail <bilbisli@gmail.com>
2020-12-05 23:23:28 +0200

    for presentation finale

--- ten_yad/register/views.py
+++ ten_yad/register/views.py
@@ -9,7 +9,11 @@ def register(response):
     if response.method == "POST":
         form = RegisterForm(response.POST)
         if form.is_valid():
-            form.save()
+            new_user = form.save()
+            new_user = authenticate(username=form.cleaned_data['username'],
+                                    password=form.cleaned_data['password1'],
+                                    )
+            login(response, new_user)
         return redirect("/")
     else:
         form = RegisterForm()

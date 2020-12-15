d20808b44c929b8ea654a14d108b7e444a6a18e5
israel avihail <bilbisli@gmail.com>
2020-12-15 14:18:14 +0200

    resetting main

--- ten_yad/homepage/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url, include
-from django.urls import path, include
-from .views import *
-
-urlpatterns = [
-    url(r'^$', homepage, name='home'),
-    url(r'posts/post', post, name='post'),
-    url(f'user/profile', user_profile, name='profile'),
-]

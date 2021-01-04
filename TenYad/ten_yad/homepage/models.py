7cd4b1d104c8ee7d7281510b68fdb5181b669794
israel avihail <bilbisli@gmail.com>
2021-01-05 00:57:44 +0200

    progrees with unit testing

--- TenYad/ten_yad/homepage/models.py
+++ TenYad/ten_yad/homepage/models.py
@@ -19,12 +19,6 @@ XL_STRING = 511
 MAX_STRING = 1023
 
 
-# class Gender(models.Model):
-#     gender = models.CharField(max_length=MIN_STRING)
-#
-#     def __str__(self):
-#         return f'{self.gender}'
-
 class Message(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='notifications', null=True)
     notification = models.CharField('notification', max_length=LARGE_STRING, blank=True, default='')
---
+++ TenYad/ten_yad/homepage/test_urls.py
@@ -0,0 +1,14 @@
+import unittest
+from django.urls import reverse, resolve
+from .views import post_page
+
+
+class TestUrls(unittest.TestCase):
+    def test_something(self):
+        url = reverse('post_page')
+        print(resolve(url))
+        assert 2 == 2
+
+
+if __name__ == '__main__':
+    unittest.main()

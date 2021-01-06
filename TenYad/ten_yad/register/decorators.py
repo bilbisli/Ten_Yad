7a39144b0e6ec57a926e13d3b42cdf9aa7f4465e
Daniel <danieldahan220715@gmail.com>
2021-01-06 22:58:57 +0200

    finish user blocking

--- TenYad/ten_yad/register/decorators.py
+++ TenYad/ten_yad/register/decorators.py
@@ -1,13 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import redirect
 
-def unathenticated_user(view_func):
+def unauthenticated_user(view_func):
     def wrapper_func(request,*args,**kwargs):
-        if request.user.is_unathenticated:
-            return redirect('home')
-        else:
 
-             return view_func(request,*args,**kwargs)
+        if unauthenticated_user():
+            if request.user.is_unathenticated:
+                return redirect('home')
+            else:
+                 return view_func(request,*args,**kwargs)
     return wrapper_func
 
 def allowed_users(allowed_roles=[]):

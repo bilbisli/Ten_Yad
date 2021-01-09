1bce11b0ade6c1dafb7a154294eea67645cc8baf
israel avihail <bilbisli@gmail.com>
2021-01-09 19:21:45 +0200

    commit before starting deployment

--- TenYad/ten_yad/ten_yad/settings.py
+++ TenYad/ten_yad/ten_yad/settings.py
@@ -14,7 +14,6 @@ from pathlib import Path
 import os
 from dotenv import load_dotenv
 
-
 load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -140,7 +139,6 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = "/"
 
 LOGOUT_REDIRECT_URL = "/"
-
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
